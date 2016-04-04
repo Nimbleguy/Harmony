a9eadcb4814cad8af9275e0d5b8db3238dc89c96
Nimbleguy <nomblest@gmail.com>
2016-04-04 13:33:23 -0400

    Some ext2 structs.

--- src/Harmony/ext2.h
+++ src/Harmony/ext2.h
@@ -6,12 +6,66 @@
 #include "multiboot.h"
 #include "utilities.h"
 
-struct superblock{
+struct superblockExt;
 
+struct superblock{
+	unsigned int inodes;
+	unsigned int blocks;
+	unsigned int resBlocks; //Su reserved blocks.
+	unsigned int unallocBlocks;
+	unsigned int unallocInodes;
+	unsigned int superblock; //Superblock block location.
+	unsigned int logBlock; //1024 << (x) to get block size.
+	unsigned int logFragment; //1024 << (x) to get fragment size.
+	unsigned int blockGroupSize; //Blocks in a block group.
+	unsigned int fragmentGroupSize; //Fragments in a block group.
+	unsigned int inodeGroupSize; //Inodes in a block group.
+	unsigned int mountTime; //POSIX last mount time.
+	unsigned int writeTime; //POSIX last write time.
+	unsigned short fsckLast; //Amount of mount times since last fsck.
+	unsigned short fsckMax; //Amount of mounts before fsck must be done.
+	unsigned short signature; //0xEF53
+	unsigned short fsState; //FS state. 1 is clean; 2 is errored.
+	unsigned short errorDo; //What to do when bad things(tm). 1 is ignore, 2 is remount as ro, 3 is kernel panic.
+	unsigned short verMinor; //Minor version.
+	unsigned int fsckTime; //POSIX last fsck time.
+	unsigned int fsckInterval; //POSIX interval between forced fscks.
+	unsigned int osId; //ID of creator OS. 0 is linux, 1 is hurd, 2 is masix, 3 is freebsd, 4 is lites.
+	unsigned int verMajor; //Major version.
+	unsigned short superUser; //UID of su.
+	unsigned short superGroup; //GID of su.
+	struct superblockExt ext; //Extension of the superblock. See struct superblockExt.
 }__attribute__((packed));
 
-struct superblockExt{
+struct superblockExt{ //Only exists if superblock->verMajor >= 1.
+	unsigned int nosuInode; //First non-reserved inode. 11 when nonexistant.
+	unsigned short inodeSize; //Byte size of inodes. 128 when nonexistant.
+	unsigned short blockGroup; //Block group the superblock is part of, if a backup copy.
+	unsigned int optional; //Optional features present. 0x1=prealloc, 0x2=afs inodes, 0x4=journaled, 0x8=xattr, 0x10=resizable, 0x20=hashindex.
+	unsigned int required; //Required features present. 0x1=compression, 0x2=typed dirs, 0x4=journal replay, 0x8=jounalUsed.
+	unsigned int rofeat; //Readonly features present. 0x1=sparse superblock & tables, 0x2=64-bit, 0x4=binary tree.
+	unsigned char fsId[16]; //Filesystem ID.
+	unsigned char volId[16]; //Volume name, terminated with \0.
+	unsigned char path[64]; //Path volume was last mounted to, terminated with \0.
+	unsigned int compress; //What compression algorithims used if superblockExt->required & 0x1.
+	unsigned char preallocFile; //Blocks to preallocate for files.
+	unsigned char preallocDir; //Blocks to preallocate for directories.
+	unsigned short reserved1; //Reserved.
+	unsigned char journalId[16]; //Same as superblockExt->fsId.
+	unsigned int journalInode; //Inode location of the journal.
+	unsigned int journalDevice; //Block device of the journal.
+	unsigned int orphanInode; //Head of the orphan inode list.
+	unsigned char reserved2[788]; //Reserved.
+}__attribute__((packed));
 
+struct groupDesc{
+	unsigned int blockAddr; //Block address of the block usage bitmap.
+	unsigned int inodeAddr; //Block address of the inode usage bitmap.
+	unsigned int inodeTable; //Block address of inode table.
+	unsigned short unallocBlocks; //Unallocated blocks in group.
+	unsigned short unallocInodes; //Unallocated inodes in group.
+	unsigned short dirs; //Number of directories.
+	unsigned char reserved[14]; //Reserved.
 }__attribute__((packed));
 
 //True if initialized successfully.
