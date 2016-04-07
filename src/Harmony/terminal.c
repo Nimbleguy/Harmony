1307b9ef1260239b47d3305ee95ff0844ac7a093
Nimbleguy <nomblest@gmail.com>
2016-04-06 22:29:27 -0400

    Revert "Attempted to do a framebuffer thing."
    This reverts commit c817458dc9fef92e9180b1905e1f621b53e8dad8.

--- src/Harmony/terminal.c
+++ src/Harmony/terminal.c
@@ -3,9 +3,6 @@
 /* The address of the framebuffer in memory. */
 char *framebuffer = (char *) 0xC00B8010;
 
-//Other framebuffer.
-struct fbText frame[(MAXROW + 1) * (MAXCOLUMN + 1)] = {0};
-
 /* Maximum column in 79. Maximum row is 24. */
 unsigned int column = 0;
 unsigned int row = 0;
@@ -30,17 +27,20 @@ void fbWrite(char* s, unsigned char fg, unsigned char bg){
 	for(unsigned int i = 0; s[i] != '\0'; i++){
 		if(s[i] == '\n'){
 			if(row == MAXROW){
-				fbScroll();
+				fbClear(lccolor);
+				row = 0;
 			}
 			else{
 				row++;
 			}
 			column = 0;
-			moveCursor((row * (MAXCOLUMN + 1)) + column);
+			moveCursor((row * 80) + column);
 			continue;
 		}
 		if(row == MAXROW && column == MAXCOLUMN){
-			fbScroll();
+			fbClear(lccolor);
+			column = 0;
+			row = 0;
 		}
 		if(column == MAXCOLUMN){
                         column = 0;
@@ -57,51 +57,21 @@ void fbWrite(char* s, unsigned char fg, unsigned char bg){
 			else{
 				column--;
 			}
-			fbChar(' ', fg, bg);
-			fbMove(row, column);
+			writeChar(2 * ((row * 80) + column - 8), ' ', fg, bg);
+			moveCursor((row * 80) + column);
 			continue;
 		}
-		fbChar(s[i], fg, bg);
-		fbMove(row, column);
+		writeChar(2 * ((row * 80) + column - 8), s[i], fg, bg);
+		moveCursor((row * 80) + column + 1);
 		column++;
 	}
-}
-
-void fbChar(char c, unsigned char fg, unsigned char bg){
-	frame[(row * 80) + column] = (struct fbText){.c = c, .forecolor = fg, .backcolor = bg};
-}
-
-void fbSync(){
-	for(unsigned int c = 0; c < MAXCOLUMN + 1; c++){
-		for(unsigned int r = 0; r < MAXROW + 1; r++){
-			struct fbText t = frame[(r * 80) + column];
-			writeChar(2 * ((r * (MAXCOLUMN + 1)) + c - 8), t.c, t.forecolor, t.backcolor);
-		}
-	}
-	moveCursor((row * (MAXCOLUMN + 1)) + column + 1);
-}
-
-void fbScroll(){
-	fbMove(0, 0);
-	return;
-
-	for(unsigned int r = 0; r < (MAXROW + 1) - 1; r++){ //-1 because last is done manually.
-		for(unsigned int c = 0; c < (MAXCOLUMN + 1); c++){
-			frame[(r * (MAXCOLUMN + 1)) + c] = frame[((r + 1) * (MAXCOLUMN + 1)) + c];
-		}
-	}
-	for(unsigned int c = 0; c < (MAXCOLUMN + 1); c++){
-		frame[(MAXROW * (MAXCOLUMN + 1)) + c] = (struct fbText){.c = ' ', .forecolor = DTCOLOR, .backcolor = BLACK};
-	}
-	fbMove(MAXROW, 0);
-	fbSync();
 }
 
 void fbClear(unsigned char color){
 	for(unsigned int c = 0; c < MAXCOLUMN + 1; c++){
 		// Insert C++ pun here
 		for(unsigned int r = 0; r < MAXROW + 1; r++){
-			frame[(r * (MAXCOLUMN + 1)) + c] = (struct fbText){.c = ' ', .forecolor = DTCOLOR, .backcolor = color};
+			writeChar(2 * ((r * 80) + c - 8), ' ', DTCOLOR, color);
 		}
 	}
 	moveCursor(0);
@@ -113,5 +83,5 @@ void fbClear(unsigned char color){
 void fbMove(unsigned int r, unsigned int c){
 	row = r;
 	column = c;
-	moveCursor((row * (MAXCOLUMN + 1)) + column + 1);
+	moveCursor((row * 80) + column + 1);
 }
